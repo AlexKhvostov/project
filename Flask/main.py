6a26acf238529109c9a845873527daf190c649b5
AlexKhvostov <Lihach.ok@gmail.com>
2019-07-02 17:45:38 +0300

    02.07 home

--- Flask/main.py
+++ Flask/main.py
@@ -10,10 +10,6 @@ logging.basicConfig(
     format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
     level=logging.DEBUG)
 
-logging.debug("This is a debug message")
-logging.info("Informational message")
-logging.error("An error has happened!")
-
 
 # код для запуска сервиса
 if __name__ == '__main__':
--- web_app/main.py
+++ web_app/main.py
@@ -7,9 +7,6 @@ logging.basicConfig(
     format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
     level=logging.DEBUG)
 
-logging.debug("This is a debug message")
-logging.info("Informational message")
-logging.error("An error has happened!")
 
 if __name__ == '__main__':
     app.run(port=5020)
